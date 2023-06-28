386dd601dcb79516c621401c0e115cb15bfdeecb
uki <hello@uki.dev>
2023-06-29 01:05:52 +1000

    add window resizing and redrawing

--- src/main.rs
+++ src/main.rs
@@ -95,26 +95,16 @@ async fn main() {
     });
 
     let size = window.inner_size();
-    surface.configure(
-        &device,
-        &SurfaceConfiguration {
-            usage: TextureUsages::RENDER_ATTACHMENT,
-            format: surface_formats,
-            width: size.width,
-            height: size.height,
-            present_mode: PresentMode::AutoNoVsync,
-            alpha_mode: surface_capabilities.alpha_modes[0],
-            view_formats: vec![],
-        },
-    );
-
-    let current_texture = surface
-        .get_current_texture()
-        .expect("Failed to get current texture");
-
-    let view = current_texture
-        .texture
-        .create_view(&TextureViewDescriptor::default());
+    let mut surface_configuration = SurfaceConfiguration {
+        usage: TextureUsages::RENDER_ATTACHMENT,
+        format: surface_formats,
+        width: size.width,
+        height: size.height,
+        present_mode: PresentMode::AutoNoVsync,
+        alpha_mode: surface_capabilities.alpha_modes[0],
+        view_formats: vec![],
+    };
+    surface.configure(&device, &surface_configuration);
 
     let camera = Camera::new();
     let inverse_view_projection = (camera.view() * camera.projection()).inversed();
@@ -139,32 +129,51 @@ async fn main() {
         label: None,
     });
 
-    let mut command_encoder =
-        device.create_command_encoder(&CommandEncoderDescriptor { label: None });
-    {
-        let mut render_pass = command_encoder.begin_render_pass(&RenderPassDescriptor {
-            label: None,
-            color_attachments: &[Some(RenderPassColorAttachment {
-                view: &view,
-                resolve_target: None,
-                ops: Operations {
-                    load: LoadOp::Clear(Color::TRANSPARENT),
-                    store: true,
-                },
-            })],
-            depth_stencil_attachment: None,
-        });
-        render_pass.set_bind_group(0, &bind_group, &[]);
-        render_pass.set_pipeline(&render_pipeline);
-        render_pass.draw(0..6, 0..1);
-    }
-
-    queue.submit(Some(command_encoder.finish()));
-    current_texture.present();
-
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
         match event {
+            Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                ..
+            } => {
+                surface_configuration.width = size.width;
+                surface_configuration.height = size.height;
+                surface.configure(&device, &surface_configuration);
+                window.request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                let current_texture = surface
+                    .get_current_texture()
+                    .expect("Failed to get current texture");
+
+                let view = current_texture
+                    .texture
+                    .create_view(&TextureViewDescriptor::default());
+
+                let mut command_encoder =
+                    device.create_command_encoder(&CommandEncoderDescriptor { label: None });
+                {
+                    let mut render_pass =
+                        command_encoder.begin_render_pass(&RenderPassDescriptor {
+                            label: None,
+                            color_attachments: &[Some(RenderPassColorAttachment {
+                                view: &view,
+                                resolve_target: None,
+                                ops: Operations {
+                                    load: LoadOp::Clear(Color::TRANSPARENT),
+                                    store: true,
+                                },
+                            })],
+                            depth_stencil_attachment: None,
+                        });
+                    render_pass.set_bind_group(0, &bind_group, &[]);
+                    render_pass.set_pipeline(&render_pipeline);
+                    render_pass.draw(0..6, 0..1);
+                }
+
+                queue.submit(Some(command_encoder.finish()));
+                current_texture.present();
+            }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 window_id,
