9ffcfc7159ee690f303e846dceb8ad45a9f58718
uki <hello@uki.dev>
2023-07-07 01:35:02 +1000

    remove dead code

--- src/engine/simulation/cell.rs
+++ src/engine/simulation/cell.rs
@@ -3,10 +3,7 @@ use bytemuck::{Pod, Zeroable};
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
 pub struct Cell {
-    // TODO: wgsl does not support u8 so until we work out how to decode bytes into `pub material: Material` correctly, simply use this
     material: u32,
-    // TODO: need to add padding so vec3 aligns
-    // pub velocity: Vec3,
 }
 impl Cell {
     // pub fn material(&self) -> Material {
